6dca01271429b288d8d64abb03d550b9b5c963e4
KRJadeja <75047189+KRJadeja@users.noreply.github.com>
2021-08-06 13:30:02 -0700

    Update CommonSteps.java

--- src/test/java/StepDefinitions/CommonSteps.java
+++ src/test/java/StepDefinitions/CommonSteps.java
@@ -11,8 +11,7 @@ public class CommonSteps {
     @Before
     public void setUp()
     {
-       // System.setProperty("webdriver.chrome.driver","C:\\Users\\KJadeja\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+       System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
         driver=new ChromeDriver();
         driver.manage().window().maximize();
     }
