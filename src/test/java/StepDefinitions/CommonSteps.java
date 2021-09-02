a295c17250b1ba02f7810604d717612baba136e5
KJadeja <kiranpzala@gmail.com>
2021-09-02 12:49:36 -0700

    CommonSteps - Saucelab

--- src/test/java/StepDefinitions/CommonSteps.java
+++ src/test/java/StepDefinitions/CommonSteps.java
@@ -3,37 +3,45 @@ package StepDefinitions;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class CommonSteps {
 
     private WebDriver driver;
+    public static final String USERNAME = "oauth-kiranpzala-7937b";
+    public static final String ACCESS_KEY = "2a485bfc-2ae9-41e9-9501-fe583a979946";
+    public static final String URL = "https://"+USERNAME+":"+ACCESS_KEY+"@ondemand.saucelabs.com:443/wd/hub";
 
     @Before
-    WebDriver getDriver()
-    {
-           /* ChromeOptions chromeOptions = new ChromeOptions();
-            WebDriverManager.chromedriver().setup();
-            WebDriver driver = new ChromeDriver(chromeOptions);*/
+    WebDriver getDriver() throws MalformedURLException {
+       /* MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
+        sauceOptions.setCapability("access_key", System.getenv("SAUCE_ACCESS_KEY"));
+        //sauceOptions.setCapability("name", testInfo.getDisplayName());
+        sauceOptions.setCapability("browserVersion", "latest");
 
-        //WebDriverManager.chromedriver().version("91.0.4472.101").setup();
-       /* WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
-        driver.manage().window().maximize();*/
-        //driver.manage().deleteAllCookies();
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("sauce:options", sauceOptions);
+        URL url = new URL("https://ondemand.us-west-1.saucelabs.com/wd/hub");*/
 
-        //driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-       // driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("version", "latest");
+        options.setCapability("platform","Windows 10");
+        options.setCapability("name","Test1");
+        //DesiredCapabilities caps = new DesiredCapabilities.chrome();
 
-       // driver.manage().window().maximize();
+        driver = new RemoteWebDriver(new URL(URL), options);
 
-          System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
-            driver=new ChromeDriver();
-            driver.manage().window().maximize();
         return driver;
     }
 
