ea616298d8389e9a115340429a6d4ca29653712d
KJadeja <kiranpzala@gmail.com>
2021-09-02 13:09:54 -0700

    CommonSteps-without sauce

--- src/test/java/StepDefinitions/CommonSteps.java
+++ src/test/java/StepDefinitions/CommonSteps.java
@@ -2,46 +2,34 @@ package StepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-//import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
-public class CommonSteps {
+public class CommonSteps{
 
     private WebDriver driver;
-    public static final String USERNAME = "oauth-kiranpzala-7937b";
-    public static final String ACCESS_KEY = "2a485bfc-2ae9-41e9-9501-fe583a979946";
-    public static final String URL = "https://"+USERNAME+":"+ACCESS_KEY+"@ondemand.saucelabs.com:443/wd/hub";
 
     @Before
-    WebDriver getDriver() throws MalformedURLException {
-       /* MutableCapabilities sauceOptions = new MutableCapabilities();
-        sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
-        sauceOptions.setCapability("access_key", System.getenv("SAUCE_ACCESS_KEY"));
-        //sauceOptions.setCapability("name", testInfo.getDisplayName());
-        sauceOptions.setCapability("browserVersion", "latest");
+    WebDriver getDriver()
+    {
+           /* ChromeOptions chromeOptions = new ChromeOptions();
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver(chromeOptions);*/
 
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability("sauce:options", sauceOptions);
-        URL url = new URL("https://ondemand.us-west-1.saucelabs.com/wd/hub");*/
+        //WebDriverManager.chromedriver().version("91.0.4472.101").setup();
+       /* WebDriverManager.chromedriver().setup();
+        this.driver = new ChromeDriver();
+        driver.manage().window().maximize();*/
+        //driver.manage().deleteAllCookies();
 
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability("version", "latest");
-        options.setCapability("platform","Windows 10");
-        options.setCapability("name","Test1");
-        //DesiredCapabilities caps = new DesiredCapabilities.chrome();
+        //driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+       // driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-        driver = new RemoteWebDriver(new URL(URL), options);
+       // driver.manage().window().maximize();
 
+          System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+            driver=new ChromeDriver();
+            driver.manage().window().maximize();
         return driver;
     }
 
