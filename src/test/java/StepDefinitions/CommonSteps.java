fe4fab54755fdb9e71a5ab98e0993e9c0564125b
KJadeja <kiranpzala@gmail.com>
2021-09-29 15:08:06 -0700

    commonsteps

--- src/test/java/StepDefinitions/CommonSteps.java
+++ src/test/java/StepDefinitions/CommonSteps.java
@@ -1,49 +1,112 @@
 package StepDefinitions;
 
+import com.saucelabs.junit.SauceOnDemandTestWatcher;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+import java.io.*;
+import java.util.Properties;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class CommonSteps{
+import com.saucelabs.saucerest.SauceREST;
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+//import org.openqa.selenium.remote.SessionId;
+//import com.saucelabs.testng.SauceOnDemandAuthenticationProvider;
 
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+//public class CommonSteps implements SauceOnDemandSessionIdProvider, SauceOnDemandAuthenticationProvider {
+  public class CommonSteps implements SauceOnDemandSessionIdProvider {
     private WebDriver driver;
+    Scenario scenario;
+
+    public static final String USERNAME = System.getenv("SAUCE_USERNAME");
+    public static final String ACCESS_KEY =System.getenv("SAUCE_ACCESS_KEY");
+    public static final String URL = "https://"+USERNAME+":"+ACCESS_KEY+"@ondemand.saucelabs.com:443/wd/hub";
+
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication(USERNAME,ACCESS_KEY);
+    private final ThreadLocal<WebDriver> webDriver = new ThreadLocal<WebDriver>();
+    private final ThreadLocal<String> sessionId = new ThreadLocal<String>();
 
     @Before
-    WebDriver getDriver()
-    {
-           /* ChromeOptions chromeOptions = new ChromeOptions();
-            WebDriverManager.chromedriver().setup();
-            WebDriver driver = new ChromeDriver(chromeOptions);*/
+    public void getScenarioName(Scenario scenario) {
+        this.scenario = scenario;
 
-        //WebDriverManager.chromedriver().version("91.0.4472.101").setup();
-       /* WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
+        System.out.println("Sessionid "+ sessionId);
+        System.out.println("Authentication "+ authentication);
+    }
+    @Before
+    WebDriver getDriver() throws MalformedURLException {
+        //To Run on Local
+       /*System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        driver=new ChromeDriver();
         driver.manage().window().maximize();*/
+
+        //To Run WebDriverManager
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        this.driver = new ChromeDriver(chromeOptions);
+        //webDriver.set(new ChromeDriver(chromeOptions));
+        driver.manage().window().maximize();
+        //WebDriverManager.chromedriver().version("91.0.4472.101").setup();
+        //driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         //driver.manage().deleteAllCookies();
 
-        //driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-       // driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        //To Run on Saucelabs
+        /*ChromeOptions options = new ChromeOptions();
+        options.setCapability("version", "latest");
+        options.setCapability("platform","Windows 10");
+        options.setCapability("name",scenario.getName());
+        //options.setCapability("name","test1");
+        options.setCapability("build","kiran");
+        //DesiredCapabilities caps = new DesiredCapabilities.chrome();
 
-       // driver.manage().window().maximize();
+        //driver = new RemoteWebDriver(new URL(URL), options);
+        webDriver.set(new RemoteWebDriver(new URL(URL), options));*/
+        //return webDriver.get();
 
-          System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
-            driver=new ChromeDriver();
-            driver.manage().window().maximize();
-        return driver;
+        //Geting System Environment from pom file
+       /* if(System.getProperty("myname").equals("Kiran"))
+            System.out.println("Environment :" +System.getProperty("myname") );*/
+        //String id = driver.session_id;
+       return driver;
     }
+
+    //@DataProvider(name= "DFDF")
 
     /*public WebDriver getDriver()
     {
         return driver;
     }*/
-
     @After
     public void tierDown()
     {
-        //WebDriver driver = getDriver();
+       // webDriver.get().quit();
         driver.quit();
     }
+    @Rule
+    public SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this,authentication);
 
+    @Override
+    public String getSessionId() {
+        return sessionId.get();
+    }
+    /*public SauceREST getSauceRest()
+    {
+        return this.sauceRest();
+    }*/
+    public SauceOnDemandAuthentication getAuthentication() {
+        return authentication;
+    }
 }
-
