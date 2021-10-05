cf62c979f48a009d3ae792d077bfa3527e25a273
KJadeja <kiranpzala@gmail.com>
2021-10-05 16:14:28 -0700

    Commonstp

--- src/test/java/StepDefinitions/CommonSteps.java
+++ src/test/java/StepDefinitions/CommonSteps.java
@@ -18,7 +18,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import com.saucelabs.saucerest.SauceREST;
 import com.saucelabs.common.SauceOnDemandAuthentication;
 import com.saucelabs.common.SauceOnDemandSessionIdProvider;
-import org.openqa.selenium.remote.SessionId;
 //import org.openqa.selenium.remote.SessionId;
 //import com.saucelabs.testng.SauceOnDemandAuthenticationProvider;
 
@@ -29,21 +28,19 @@ import java.net.URL;
 //public class CommonSteps implements SauceOnDemandSessionIdProvider, SauceOnDemandAuthenticationProvider {
  public class CommonSteps implements SauceOnDemandSessionIdProvider {
 //public class CommonSteps {
-    //private WebDriver driver;
-    //Scenario scenario;
+    private WebDriver driver;
+    Scenario scenario;
 
     public static final String USERNAME = System.getenv("SAUCE_USERNAME");
     public static final String ACCESS_KEY =System.getenv("SAUCE_ACCESS_KEY");
+    public static final String URL = "https://"+USERNAME+":"+ACCESS_KEY+"@ondemand.saucelabs.com:443/wd/hub";
+
     public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication(USERNAME,ACCESS_KEY);
-
-    public final String URL = "https://"+ authentication.getUsername()+":"+authentication.getAccessKey()+"@ondemand.saucelabs.com:443/wd/hub";
-
-
-    private ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
-    private ThreadLocal<String> sessionId = new ThreadLocal<String>();
+    private final ThreadLocal<WebDriver> webDriver = new ThreadLocal<WebDriver>();
+    private final ThreadLocal<String> sessionId = new ThreadLocal<String>();
 
     public String jobName;
-    //String id;
+    String id;
 
     @Before(order = 0)
     public void getScenarioName(Scenario scenario) {
@@ -52,12 +49,12 @@ import java.net.URL;
        // System.out.println("Authentication "+ authentication);
     }
 
-    /*@Before
-    public void setUp() throws MalformedURLException {
+    @Before
+    public WebDriver getDriver() throws MalformedURLException {
         //To Run on Local
-       *//*System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+       /*System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
         driver=new ChromeDriver();
-        driver.manage().window().maximize();*//*
+        driver.manage().window().maximize();*/
 
         //To Run WebDriverManager
         //ChromeOptions chromeOptions = new ChromeOptions();
@@ -80,50 +77,29 @@ import java.net.URL;
         //DesiredCapabilities caps = new DesiredCapabilities.chrome();
 
         //driver = new RemoteWebDriver(new URL(URL), options);
-        driver.set(new RemoteWebDriver(new URL(URL), options));
-        //webDriver.set(new RemoteWebDriver(new URL(URL), options));
-        //id = ((RemoteWebDriver) getDriver()).getSessionId().toString();
-        sessionId.set(((RemoteWebDriver) getDriver()).getSessionId().toString());
-        System.out.println("Session ID : "+getSessionId());
-       // return webDriver.get();
+        webDriver.set(new RemoteWebDriver(new URL(URL), options));
+        id = ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+        sessionId.set(id);
+        return webDriver.get();
 
         //Geting System Environment from pom file
-       *//* if(System.getProperty("myname").equals("Kiran"))
-            System.out.println("Environment :" +System.getProperty("myname") );*//*
+       /* if(System.getProperty("myname").equals("Kiran"))
+            System.out.println("Environment :" +System.getProperty("myname") );*/
         //String id = driver.session_id;
        //return driver;
-    }*/
+    }
 
     //@DataProvider(name= "DFDF")
 
-    public WebDriver getDriver() throws MalformedURLException {
-        //To Run on Saucelabs
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability("version", "latest");
-        options.setCapability("platform","Windows 10");
-        options.setCapability("name",jobName);
-        //options.setCapability("name","test1");
-        options.setCapability("build","kiran");
-        //DesiredCapabilities caps = new DesiredCapabilities.chrome();
-
-        //driver = new RemoteWebDriver(new URL(URL), options);
-        driver.set(new RemoteWebDriver(new URL(URL), options));
-        //webDriver.set(new RemoteWebDriver(new URL(URL), options));
-        //id = ((RemoteWebDriver) getDriver()).getSessionId().toString();
-        sessionId.set(((RemoteWebDriver) driver.get()).getSessionId().toString());
-        System.out.println("Session ID : "+getSessionId());
-        // return webDriver.get();
-        return driver.get();
-        //return driver;
+    public WebDriver getWebDriver()
+    {
+        return webDriver.get();
     }
     @After
     public void tierDown()
     {
-       // id = ((RemoteWebDriver) getDriver()).getSessionId().toString();
-        //sessionId.set(id);
-        //driver.quit();
-        //webDriver.get().quit();
-        driver.get().quit();
+
+        webDriver.get().quit();
     }
     @Rule
     public SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this,authentication);
@@ -133,8 +109,8 @@ import java.net.URL;
 
         return sessionId.get();
     }
-    /*public SauceOnDemandAuthentication getAuthentication() {
+    public SauceOnDemandAuthentication getAuthentication() {
 
         return authentication;
-    }*/
+    }
 }
