25f514368a08b2396789a58fcb10add73a59638d
KJadeja <kiranpzala@gmail.com>
2022-03-10 16:32:52 -0700

    TestRunner...

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         glue = "StepDefinitions",
         plugin = {"pretty", "html:target/Report/report.html"},
         //plugin = {"pretty", "json:target/Report/report.json"},
-        tags="@smoke",
+        tags="@smoke and @H1",
         publish = false,
         dryRun = false
 )
