bddd95c9b0daf2bc8dc52a2feaa1a91e518c66aa
KJadeja <kiranpzala@gmail.com>
2022-12-20 14:49:58 -0700

    test runner arge

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -8,9 +8,9 @@ import org.junit.runner.RunWith;
         //features = {"classpath:NewFeatures/Sample.feature"},
         features = {"src/test/java/Features/EbayHome.feature"},
         glue = "StepDefinitions",
-        plugin = {"pretty", "html:target/Report/report.html"},
-        //plugin = {"pretty", "json:target/Report/report.json"},
-        tags="@H1 or @H2",
+        //plugin = {"pretty", "html:target/Report/report.html","StepDefinitions.BaseClass"},
+        plugin = {"pretty", "json:target/cucumber.json","html:target/Report/cucumber.html","junit:target/cucumber-results.xml","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        tags="@e1 or @e2",
         publish = false,
         dryRun = false
 )
